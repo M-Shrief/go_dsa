ceeb14e2d1ed23c66aa1c8adcf7f31ea3923d7fc
M-Shrief <m.shrief.dev@proton.me>
2024-02-10 12:04:02 +0200

    encapsulated QuickSort() inner functions

--- sorting/quickSort.go
+++ sorting/quickSort.go
@@ -3,24 +3,24 @@ package sorting
 import "golang.org/x/exp/constraints"
 
 func QuickSort[T constraints.Ordered](arr []T) []T {
-	QuickSortRange[T](arr, 0, len(arr)-1)
+	quickSortRange[T](arr, 0, len(arr)-1)
 	return arr
 }
 
-func QuickSortRange[T constraints.Ordered](arr []T, low, high int) {
+func quickSortRange[T constraints.Ordered](arr []T, low, high int) {
 	if len(arr) <= 1 {
 		return
 	}
 
 	if low < high {
 		// pIndex is the partitioning index, arr[pIndex] is now at the right place
-		pIndex := Partition[T](arr, low, high)
-		QuickSortRange[T](arr, low, pIndex-1)
-		QuickSortRange[T](arr, pIndex+1, high)
+		pIndex := partition[T](arr, low, high)
+		quickSortRange[T](arr, low, pIndex-1)
+		quickSortRange[T](arr, pIndex+1, high)
 	}
 }
 
-func Partition[T constraints.Ordered](arr []T, low, high int) int {
+func partition[T constraints.Ordered](arr []T, low, high int) int {
 	pivot := arr[high]
 	index := low - 1
 
